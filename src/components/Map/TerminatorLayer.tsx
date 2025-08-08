c6f0fc8c2fe26626ba8df9fd9fe579981cb1ffb5
Uspectacle <super.mario.larsen@gmail.com>
2025-08-09 01:17:57 +0200

    change terminator color

--- src/components/Map/TerminatorLayer.tsx
+++ src/components/Map/TerminatorLayer.tsx
@@ -3,7 +3,7 @@ import { useMap } from "react-leaflet";
 import terminator, { Terminator } from "@joergdietrich/leaflet.terminator";
 
 interface TerminatorLayerProps {
-  date?: Date;
+  date: Date;
 }
 
 const TerminatorLayer: React.FC<TerminatorLayerProps> = ({ date }) => {
@@ -11,30 +11,19 @@ const TerminatorLayer: React.FC<TerminatorLayerProps> = ({ date }) => {
   const terminatorRef = useRef<Terminator | null>(null);
 
   useEffect(() => {
-    const term = terminator();
+    const term = terminator({
+      fillColor: "rgba(9, 0, 27, 0.8)",
+    });
     terminatorRef.current = term;
     term.addTo(map);
 
-    if (date) {
-      term.setTime(date);
-    }
-
-    const interval = setInterval(() => {
-      if (!date) {
-        term.setTime(new Date());
-      }
-    }, 60 * 1000);
-
     return () => {
       map.removeLayer(term);
-      clearInterval(interval);
     };
   }, [map]);
 
   useEffect(() => {
-    if (terminatorRef.current && date) {
-      terminatorRef.current.setTime(date);
-    }
+    terminatorRef.current?.setTime(date);
   }, [date]);
 
   return null;
